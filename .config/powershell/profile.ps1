fe9edb5d7fef86a8cd2eda74774d3f2f0b07e259
Ben Wilkinson <benwilk@microsoft.com>
2021-08-11 18:43:07 -0700

    update profile

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -3,5 +3,4 @@ Write-Warning -Message "I am user shared $($PSVersionTable.PSVersion.ToString())
 if ($PSVersionTable.psversion.Major -ge 6)
 { Import-Module oh-my-posh -MinimumVersion 3.0 ; Set-PoshPrompt -Theme $home\.config\my-oh-my-posh.json }
 Import-Module posh-git
-Import-Module brwazure
 Import-Module Terminal-Icons
