39491b6473e1653fbe3e9718fb7e691870685436
Ben Wilkinson <benwilk@microsoft.com>
2021-08-14 13:52:01 -0700

    disable oh-my-posh

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,6 +1,6 @@
 Write-Warning -Message "I am user shared $($PSVersionTable.PSVersion.ToString())"
 
 if ($PSVersionTable.psversion.Major -ge 6)
-{ Import-Module oh-my-posh -MinimumVersion 3.0 ; Set-PoshPrompt -Theme $home/.config/my-oh-my-posh.json }
+# { Import-Module oh-my-posh -MinimumVersion 3.0 ; Set-PoshPrompt -Theme $home/.config/my-oh-my-posh.json }
 Import-Module posh-git
 Import-Module Terminal-Icons
